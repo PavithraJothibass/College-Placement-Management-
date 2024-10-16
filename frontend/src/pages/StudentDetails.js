24292b2d0eef876f70a1492d8239d48a0433a065
PavithraJothibass <165559616+PavithraJothibass@users.noreply.github.com>
2024-10-16 15:53:50 +0530

    Update StudentDetails.js

--- frontend/src/pages/StudentDetails.js
+++ frontend/src/pages/StudentDetails.js
@@ -32,7 +32,7 @@ const StudentDetailsForm = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:5000/api/studentdetails', {
+            const response = await fetch(https://college-placement-management-backend-2.onrender.com/api/studentdetails', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
